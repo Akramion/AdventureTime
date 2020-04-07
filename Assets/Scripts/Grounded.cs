d48ef4abda98ed69ffd7026d4e2c1388843fb65f
Akramion <leha.bushuev@gmail.com>
2020-04-07 15:25:19 +0500

    basic movement

---
+++ Assets/Scripts/Grounded.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+    public bool isGrounded = true;
+    
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.collider.tag == "Ground") {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+        if(collision.collider.tag == "Ground") {
+            isGrounded = false;
+        }
+    }
+}
