f72f2c23c410f4235b5cfe6c37dbd6ef8929f180
Akramion <leha.bushuev@gmail.com>
2020-04-09 13:59:37 +0500

    Tiles and Saw

---
+++ Assets/Scripts/SawController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawController : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] waypoints;
+
+    [SerializeField]
+    float moveSpeed = 2f;
+
+    int waypointIndex = 0;
+    private SceneController sceneController;
+    private void Awake() {
+        sceneController = GameObject.Find("Main Camera").GetComponent<SceneController>();
+    }
+
+    private void Start() {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    private void Update() {
+        Move();
+    }
+
+    private void Move() {
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
+        Debug.Log(waypointIndex);
+
+
+
+        if(transform.position == waypoints[waypointIndex].transform.position) {
+            waypointIndex++;
+        }
+
+        if(waypointIndex == waypoints.Length) {
+            waypointIndex = 0;
+        }
+
+        Debug.Log(transform.position);
+        Debug.Log(waypoints[waypointIndex].transform.position);
+}
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if(collider.tag == "Player") {
+            sceneController.EndGame();
+        }
+    }
+}
