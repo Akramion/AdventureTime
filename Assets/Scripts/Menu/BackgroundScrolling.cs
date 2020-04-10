08599411fb3a9df3c58b85682a33d871d93e9ca5
Fractight <goglbummm@gmail.com>
2020-04-10 14:21:31 +0500

    Scrolling background

---
+++ Assets/Scripts/Menu/BackgroundScrolling.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BackgroundScrolling : MonoBehaviour
+{
+    public float speed;
+    public Vector3 startPosition;
+    private float scrollHeight;
+
+    void Start()
+    {
+        scrollHeight = Camera.main.orthographicSize * 2;
+
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.size = new Vector2(spriteRenderer.size.x, spriteRenderer.size.y * 2.5f);
+
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log((startPosition.y - transform.position.y).ToString());
+        if (startPosition.y - transform.position.y >= scrollHeight)
+        {
+            transform.position = startPosition;
+        }
+
+        transform.position = new Vector3(transform.position.x, transform.position.y - speed, transform.position.z);
+
+    }
+}
