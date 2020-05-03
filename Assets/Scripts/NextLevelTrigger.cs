b9c947d88a6d9e5989313777e0a0e8f48e1e4e3b
Akramion <leha.bushuev@gmail.com>
2020-05-03 13:15:04 +0500

    Fix levels

--- Assets/Scripts/NextLevelTrigger.cs
+++ Assets/Scripts/NextLevelTrigger.cs
@@ -6,6 +6,7 @@ public class NextLevelTrigger : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collider) {
         if(collider.tag == "Player") {
+            Debug.Log(Time.timeSinceLevelLoad);
             SceneController sceneController = GameObject.Find("SceneController").GetComponent<SceneController>();
             sceneController.OpenTransitionPanel();
 
