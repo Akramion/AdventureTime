97c1fea2fb17d81507fb113a336a1648c9f6016a
Akramion <leha.bushuev@gmail.com>
2020-05-03 14:35:37 +0500

    settings

--- Assets/Scripts/SettingsController.cs
+++ Assets/Scripts/SettingsController.cs
@@ -6,7 +6,25 @@ using UnityEngine.Audio;
 public class SettingsController : MonoBehaviour {
     public AudioMixer audioMixer;
 
+    private bool buttonToggle = false;
+
+
     public void ChangeVolume(float volume) {
         audioMixer.SetFloat("MasterVol", Mathf.Log(volume) * 20);
+    }
+
+    public void SetButton() {
+        buttonToggle = !buttonToggle;
+    }
+
+    private void Update() {
+        if (Input.anyKey ** buttonToggle == true) {
+    
+            foreach(KeyCode vKey in System.Enum.GetValues(typeof(KeyCode))){
+                if(Input.GetKey(vKey)){
+                    Debug.Log(vKey);
+                }
+            }           
+        }
     }
 }
