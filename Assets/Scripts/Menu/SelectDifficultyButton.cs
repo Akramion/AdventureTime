8acd2de3e890267a077468ab922f330a7da73fd9
Shevela Roman <goglbummm@gmail.com>
2020-05-07 14:48:26 +0500

    SetDifficultyButton reference fix

---
+++ Assets/Scripts/Menu/SelectDifficultyButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectDifficultyButton : MonoBehaviour
+{
+    [SerializeField]
+    private bool isHard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneController sc = GameObject.Find("SceneController").GetComponent<SceneController>();
+        Button b = GetComponent<Button>();
+
+        b.onClick.AddListener(() => sc.SetDifficulty(isHard));
+    }
+}
